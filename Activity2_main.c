7a5e71c7cdba45a856dc803312fb035e81528eed
murali980 <muralikrishna0980@gmail.com>
2021-04-28 15:46:56 +0530

    Activity2

---
+++ Activity2_main.c
@@ -0,0 +1,13 @@
+#include "Activity2.h"
+
+int main(void)
+{
+    InitADC();
+    uint16_t temp;
+    while(1)
+    {
+        temp=ReadADC(0);
+        _delay_ms(200);
+    }
+    return 0;
+}
---
+++ inc/Activity2.h
@@ -0,0 +1,28 @@
+/*
+ */
+
+#include <avr/io.h>
+
+#define F_CPU	20000000
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+/*
+ * ADCexample.c
+ *
+ * Created: 4/13/2013 5:22:48 PM
+ *  Author: SAM
+ */
+
+#define F_CPU	20000000
+
+#include <avr/io.h>
+#include<util/delay.h>
+#include <avr/interrupt.h>
+
+void InitADC()
+uint16_t ReadADC(uint8_t ch)
+
+
+#endif /** __Activity_H_ */
