a714ce630552cf620f4225cc72280ebc49cc902c
murali980 <muralikrishna0980@gmail.com>
2021-04-29 09:58:50 +0530

    Activity2

--- Activity2_main.c
+++ Activity2_main.c
@@ -1,8 +1,23 @@
+  /**
+ * @file Activity2.c
+ * @author Murali ()
+ * @brief To set the temperature of an heater in a car using ADC 
+ * @date 2021-04-2
+ */
+
 #include "Activity2.h"
 
+/*header files*/
 
 
-int main(void)
+/**
+ * @brief Main function where the code execution starts
+ * @return int Return 0 if the program completes successfully
+ * @note calling the functions in main function
+ * @note calling the function and taking the values from ADC
+ */
+
+int main(void)  // main function
 {
     InitADC();
     uint16_t temp;
@@ -12,4 +27,6 @@ int main(void)
         _delay_ms(200);
     }
     return 0;
+    
+    /* Return 0 if the program complies*/
 }
